627c117dd5250e6ee4d864eed9b5cfb689eaad11
Proo2005 <prochak1922@gmail.com>
2026-01-14 00:00:11 +0530

    gigflow backend

--- routes/bid.routes.js
+++ routes/bid.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createJob, getAllJobs } from "../controllers/job.controller.js";
 
 const router = express.Router();
-
+// makae
 router.post("/create", createJob);
 router.get("/", getAllJobs);
 
